59830830028b7cb0caa452c91be9f648ded519a8
Shobhitpatel19 <shobhitp49@gmail.com>
2023-08-16 22:07:52 +0530

    added sample login

--- reelsnew/src/components/Login.jsx
+++ reelsnew/src/components/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  const trackEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const trackPassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const printDetails = () => {
+    alert(email + " " + password);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <input
+        type="email"
+        onChange={trackEmail}
+        value={email}
+        placeholder="email"
+      ></input>
+      <br></br>
+      <input
+        type="password"
+        onChange={trackPassword}
+        value={password}
+        placeholder="password"
+      ></input>
+      <br></br>
+      <button onClick={printDetails}>Login</button>
+    </>
+  );
 }
 
-export default Login
+export default Login;
