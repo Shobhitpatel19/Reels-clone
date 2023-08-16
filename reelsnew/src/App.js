93c18e261cc25613b714252930c66173f58e1842
Shobhitpatel19 <shobhitp49@gmail.com>
2023-08-16 21:47:42 +0530

    added routing

--- reelsnew/src/App.js
+++ reelsnew/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
+
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div>Reels Project</div>
+    <>
+      <Switch>
+        {/* Route -> which components should render on which path */}
+        <Route path="/feed">
+          <Feed></Feed>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/signup">
+          <Signup></Signup>
+        </Route>
+        <Route path="/profile">
+          <Profile></Profile>
+        </Route>
+        <Route>
+          <PageNotFound></PageNotFound>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
