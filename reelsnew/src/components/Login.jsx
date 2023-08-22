6add6b1ea05d94131d93b9757732b941144c2db8
Shobhitpatel19 <shobhitp49@gmail.com>
2023-08-23 00:01:07 +0530

    added login components

--- reelsnew/src/components/Login.jsx
+++ reelsnew/src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth"
+import { signInWithEmailAndPassword } from "firebase/auth";
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+  let [user, setUser] = useState(null);
+  let [loader, setLoader] = useState(false);
+  let [error, setError] = useState("");
 
   const trackEmail = (e) => {
     setEmail(e.target.value);
@@ -11,29 +14,48 @@ function Login() {
   const trackPassword = (e) => {
     setPassword(e.target.value);
   };
-  const printDetails = async function(){
-    alert(email + " " + password);
-    let userCred = await signInWithEmailAndPassword(auth, email, password);
-    console.log(userCred.user);
+  const printDetails = async function () {
+    // alert(email + " " + password);
+    try {
+      setLoader(true);
+      let userCred = await signInWithEmailAndPassword(auth, email, password);
+      // console.log(userCred.user);
+      setUser(userCred.user);
+    } catch (err) {
+      setError(err.message);
+      // After some time -> error message remove
+      setTimeout(()=>{
+        setError("")
+      },2000);
+    }
+
+    setLoader(false);
   };
 
   return (
     <>
-      <input
-        type="email"
-        onChange={trackEmail}
-        value={email}
-        placeholder="email"
-      ></input>
-      <br></br>
-      <input
-        type="password"
-        onChange={trackPassword}
-        value={password}
-        placeholder="password"
-      ></input>
-      <br></br>
-      <button onClick={printDetails}>Login</button>
+      {error != "" ? (<h1>Error is {error}</h1>) : 
+      loader == true ? (<h1>... Loading</h1>) : 
+      user != null ? (<h1>user is {user.uid}</h1>) :
+      (
+        <>
+          <input
+            type="email"
+            onChange={trackEmail}
+            value={email}
+            placeholder="email"
+          ></input>
+          <br></br>
+          <input
+            type="password"
+            onChange={trackPassword}
+            value={password}
+            placeholder="password"
+          ></input>
+          <br></br>
+          <button onClick={printDetails}>Login</button>
+        </>
+      )}
     </>
   );
 }
